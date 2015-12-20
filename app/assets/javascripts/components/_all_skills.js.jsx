980b90449f33af0c7c3bd0979ff3b124f111afe7
roseak <rose.kohn@gmail.com>
2015-12-20 12:57:08 -0700

    Add ability to add a new skill without refreshing the page.

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,12 +1,6 @@
 var AllSkills = React.createClass ({
-  componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
-  },
-  getInitialState() {
-    return { skills: [] }
-  },
   render() {
-    var skills = this.state.skills.map((skill) => {
+    var skills = this.props.skills.map((skill) => {
       return (
         <div key={skill.id}>
           <h3>{skill.name}</h3>
--- app/assets/javascripts/components/_new_skills.js.jsx
+++ app/assets/javascripts/components/_new_skills.js.jsx
@@ -1,7 +1,23 @@
 var NewSkills = React.createClass ({
+  handleClick() {
+    let name = this.refs.name.value;
+    let details = this.refs.details.value;
+
+    $.ajax({
+      url: '/api/v1/skills',
+      type: 'POST',
+      data: { skill: { name: name, details: details } },
+      success: (skill) => {
+        this.props.handleSubmit(skill);
+      }
+    });
+  },
   render() {
     return (
       <div>
+        <input ref='name' placeholder='Enter name of skill' />
+        <input ref='details' placeholder='Details' />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
